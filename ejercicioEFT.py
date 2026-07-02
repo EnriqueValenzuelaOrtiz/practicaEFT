46b083bfd6c13b28ab2ef97c1d425f4c0657bf3e
EnriqueValenzuelaOrtiz <enri.valenzuela@duocuc.cl>
2026-07-02 16:28:00 -0400

    Update ejercicioEFT.py

--- ejercicioEFT.py
+++ ejercicioEFT.py
@@ -1,4 +1,4 @@
-
+#Funciones
 def menu():
     print("============== MENÚ PRINCIPAL ==============")
     print("1. Stock por categoría")
@@ -12,7 +12,7 @@ def menu():
 def leer_opcion():
     while True:
         try:
-            opcion=int(input("Qué desea hacer?"))
+            opcion=int(input("Ingrese opción: \n"))
             if opcion<1 or opcion>6:
                 raise ValueError
             else:
@@ -20,25 +20,35 @@ def leer_opcion():
         except ValueError
             print("Error. Ingrese un caracter válido")
 
-def stock_categoria(categoria.upper(), dic_product, dict_ventas):
-    for key, value in dic_product.items():
+def stock_categoria(categoria.lower(), dic_productos, dict_ventas):
+    total_stock=0
+    for key, value in dic_productos.items():
         if value[1]==categoria:
             for key2, value2 in dic_ventas.items():
-                if value[2]>=1:
-                    print("
+                if value2[2]!=0:
+                    total+=dic_ventas[key][1]                  
+    print(f"El total de stock disponible es: {total_stock}  \n")
 
-      
+def busqueda_precio(precio_min, precio_max, dic_ventas):
+    producto_correcto=[]
+    for key, value in dic_ventas.items():
+        if value[0]>=precio_min and value[0]<=precio_max and value[1]!=0:            
+            producto_correcto.append(key)
+    print(f"Los productos encontrados son: {producto_correcto}")
+        
                   
 
 
 
 
 
+#Código principal
+dic_productos = {
 
-dic_product={
-    }
 
-dic_ventas={
-    }
+}
 
-    }
+dic_ventas = {
+
+
+}
